056f57a8f05ac976ad65dffedf0e7d8f3f5cf0d8
kevenruiz <kevenruizh@gmail.com>
2021-03-24 15:03:26 -0700

    countasayes test

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,34 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { countAsAYes } from '../utils.js';
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    
+    //const expected = true;
+    const userInput = 'yes';
+    const expected = countAsAYes(userInput);
+
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = true;
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('returns false', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    //const expected = true;
+    const userInput = 'no';
+    const expected = countAsAYes(userInput);
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = false;
 
     //Expect
     // Make assertions about what is expected versus the actual result
---
+++ utils.js
@@ -0,0 +1,17 @@
+export function countAsAYes(input) {
+    if (input.charAt(0).toLowerCase() === 'y') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+
+
+
+
+// var string = 'Yes';
+
+//string.charAt(0).LowerCase(); // Returns "F"
